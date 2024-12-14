3f6ee4dbd710237bdac5d8bf084bb3c9b319a1ca
ericafk0001 <dxslyofficial@gmail.com>
2024-12-13 20:42:51 -0500

    window

--- main.cpp
+++ main.cpp
@@ -5,41 +5,19 @@ using namespace std;
 
 int main()
 {
-
-    const int SCREEN_WIDTH = 800;
-    const int SCREEN_HEIGHT = 600;
-    int ball_x = 100;
-    int ball_y = 100;
-    int ball_speed_x = 5;
-    int ball_speed_y = 5;
-    int ball_radius = 15;
-
-    cout << "Hello World" << endl;
-
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Pong !");
+    cout << "Start" << endl;
+    const int screen_width = 1280;
+    const int screen_height = 800;
+    InitWindow(screen_width, screen_height, "Pong!");
     SetTargetFPS(60);
 
     while (WindowShouldClose() == false)
     {
-
-        ball_x += ball_speed_x;
-        ball_y += ball_speed_y;
-
-        if (ball_x + ball_radius >= SCREEN_WIDTH || ball_x - ball_radius <= 0)
-        {
-            ball_speed_x *= -1;
-        }
-
-        if (ball_y + ball_radius >= SCREEN_HEIGHT || ball_y - ball_radius <= 0)
-        {
-            ball_speed_y *= -1;
-        }
-
         BeginDrawing();
-        ClearBackground(BLACK);
-        DrawCircle(ball_x, ball_y, ball_radius, WHITE);
+
         EndDrawing();
     }
 
     CloseWindow();
+    return 0;
 }
