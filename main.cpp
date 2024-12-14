afb75ca4cba084328fc27b8f78ad49637151157a
ericafk0001 <dxslyofficial@gmail.com>
2024-12-14 16:25:13 -0500

    paddle controls

--- main.cpp
+++ main.cpp
@@ -31,7 +31,33 @@ public:
     }
 };
 
+class Paddle
+{
+public:
+    float x, y;
+    float width, height;
+    int speed;
+
+    void Draw()
+    {
+        DrawRectangle(x, y, width, height, WHITE);
+    }
+
+    void Update()
+    {
+        if (IsKeyDown(KEY_W))
+        {
+            y = y - speed;
+        }
+        if (IsKeyDown(KEY_S))
+        {
+            y = y + speed;
+        }
+    }
+};
+
 Ball ball;
+Paddle player;
 
 int main()
 {
@@ -47,17 +73,24 @@ int main()
     ball.speed_x = 7;
     ball.speed_y = 7;
 
+    player.width = 25;
+    player.height = 120;
+    player.x = screen_width - player.width - 10;
+    player.y = screen_height / 2 - player.height / 2;
+    player.speed = 6;
+
     while (WindowShouldClose() == false)
     {
         BeginDrawing();
 
         ball.Update();
+        player.Update();
 
         ClearBackground(BLACK);
         DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
         ball.Draw();
         DrawRectangle(10, screen_height / 2 - 60, 25, 120, WHITE);
-        DrawRectangle(screen_width - 35, screen_height / 2 - 60, 25, 120, WHITE);
+        player.Draw();
         EndDrawing();
     }
 
