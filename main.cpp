13a16919ba704a06db599de91196fea16d72724e
ericafk0001 <dxslyofficial@gmail.com>
2024-12-14 17:42:57 -0500

    add paddle collision change player speed 6 >> 7

--- main.cpp
+++ main.cpp
@@ -111,7 +111,7 @@ int main()
     player.height = 120;
     player.x = screen_width - player.width - 10;
     player.y = screen_height / 2 - player.height / 2;
-    player.speed = 6;
+    player.speed = 7;
 
     cpu.width = 25;
     cpu.height = 120;
@@ -127,6 +127,15 @@ int main()
         ball.Update();
         player.Update();
         cpu.Update(ball.y);
+
+        if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player.x, player.y, player.width, player.height}))
+        {
+            ball.speed_x *= -1;
+        }
+        if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{cpu.x, cpu.y, cpu.width, cpu.height}))
+        {
+            ball.speed_x *= -1;
+        }
 
         ClearBackground(BLACK);
         DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
