a6a9d6a4aacd27667fd35bb90b1e4a11442163fe
ericafk0001 <dxslyofficial@gmail.com>
2024-12-13 21:32:17 -0500

    Ball class

--- main.cpp
+++ main.cpp
@@ -3,6 +3,27 @@
 
 using namespace std;
 
+class Ball
+{
+public:
+    float x, y;
+    int speed_x, speed_y;
+    int radius;
+
+    void Draw()
+    {
+        DrawCircle(x, y, radius, WHITE);
+    };
+
+    void Update()
+    {
+        x += speed_x;
+        y += speed_y;
+    }
+};
+
+Ball ball;
+
 int main()
 {
     cout << "Start" << endl;
@@ -11,10 +32,23 @@ int main()
     InitWindow(screen_width, screen_height, "Pong!");
     SetTargetFPS(60);
 
+    ball.radius = 20;
+    ball.x = screen_width / 2;
+    ball.y = screen_height / 2;
+    ball.speed_x = 7;
+    ball.speed_y = 7;
+
     while (WindowShouldClose() == false)
     {
         BeginDrawing();
 
+        ball.Update();
+
+        ClearBackground(BLACK);
+        DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
+        ball.Draw();
+        DrawRectangle(10, screen_height / 2 - 60, 25, 120, WHITE);
+        DrawRectangle(screen_width - 35, screen_height / 2 - 60, 25, 120, WHITE);
         EndDrawing();
     }
 
